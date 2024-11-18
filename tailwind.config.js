8ae02fa13e9e5c2a54e43c707249c868cdfdcad5
Mathieu <mathieu@fellicht.nl>
2024-11-18 20:23:05 +0100

    Image, fonts, styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        background: {
+          '0%': { transform: 'scale(1.3)', opacity: '.1' },
+          '100%': { transform: 'scale(1.0)', opacity: '1' },
+        },
+        title: {
+          '0%': { transform: 'scale(1)', opacity: '.1' },
+          '100%': { transform: 'scale(1.1)', opacity: '1' },
+        },
+      },
+      animation: {
+        background: 'background 1.5s forwards ease-in-out',
+        title: 'title 1.5s forwards ease-in-out',
+      },
+    },
   },
   plugins: [],
 }
