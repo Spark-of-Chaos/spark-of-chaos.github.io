4ce882aa2c02bb6e2303bd463fa4600f4bbb00f8
Mathieu <mathieu@fellicht.nl>
2025-03-29 16:35:45 +0100

    Games kabonk

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}", "./games/**/*.{html,js}"],
   theme: {
     extend: {
     },
